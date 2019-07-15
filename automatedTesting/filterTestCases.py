6c421842f824668358a8c9de58354cc5460cc481
Jean-Pierre <jpvdheydt@googlemail.com>
2019-07-15 18:03:21 +0200

    filtered easy but not to easy testfiles. (single core williams took at most 5000 sec)$

---
+++ automatedTesting/filterTestCases.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+import errno
+import analyseUtil as util
+import constants as consts 
+
+homeDirName = consts.homeDirName
+
+for folder in os.listdir(os.path.join(homeDirName, 'result')):
+    currPath = os.path.join(homeDirName, 'result', folder)
+    testFilePath = os.path.join(homeDirName, 'allFiles', folder)
+    filteredPath = os.path.join(homeDirName, 'filtered', folder)
+    if not os.path.exists(filteredPath):
+        os.mkdir(filteredPath)
+        print("Directory " , filteredPath ,  " Created ")
+    else:    
+        print("Directory " , filteredPath ,  " already exists")
+    shutil.copy2(os.path.join(testFilePath, 'domain.pddl'), os.path.join(filteredPath, 'domain.pddl'))
+
+    for fileName in os.listdir(currPath):
+        filePath = os.path.join(currPath, fileName)
+        testFileData = os.path.join(testFilePath, fileName[:-4])
+        filteredData = os.path.join(filteredPath, fileName[:-4])
+        if fileName.startswith('p'):
+            with open(filePath, 'r') as analyseFile:
+                lines = analyseFile.readlines()
+                if util.foundValidPlan(lines) and util.getCalcTime(lines) > 5:
+                    shutil.copy2(testFileData, filteredData)
+                    #print(os.path.join(homeDirName, 'filtered', folder, fileName))
